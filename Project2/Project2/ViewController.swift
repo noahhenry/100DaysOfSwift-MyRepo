0cf6eb7fcc08466bd0ab200c5dc62ff5ec3e16db
Noah Henry <noah.henry@allscripts.com>
2019-10-11 08:53:43 -0500

    Project setup and UI layout.

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Henry, Noah on 10/11/19.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet var button1: UIButton!
+	@IBOutlet var button2: UIButton!
+	@IBOutlet var button3: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
