bb29d1b45b3d09560b154cda0ebe758e057769cd
Noah Henry <noah.henry@allscripts.com>
2019-10-16 15:56:18 -0500

    Choosing a website: UIAlertController action sheets.

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -22,11 +22,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+		
 		let url = URL(string: "https://www.hackingwithswift.com")!
 		webView.load(URLRequest(url: url))
 		webView.allowsBackForwardNavigationGestures = true
 	}
+	
+	@objc func openTapped() {
+		let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+		ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+		ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+		ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // important for iPad use.
+		
+		present(ac, animated: true)
+	}
 
-
+	func openPage(action: UIAlertAction) {
+		// let url = URL(string: "https://" + action.title!)! // doulbe force-unwrap -> 1. because we know our 'alert action' will have a title and 2. because we know the finished string will be a valid url. We could use a guard let just to be safe.
+		// guard let way
+		guard let actionTitle = action.title else { return }
+		guard let url = URL(string: "https://" + actionTitle) else { return }
+		
+		webView.load(URLRequest(url: url))
+	}
+	
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		title = webView.title
+	}
 }
 
