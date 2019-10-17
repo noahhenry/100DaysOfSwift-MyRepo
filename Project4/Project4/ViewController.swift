dfedb13bb07c5cf4cb9cf417c5677ceb46361109
Noah Henry <noah.henry@allscripts.com>
2019-10-17 10:46:46 -0500

    Typo

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
 		let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
 		
-		progressView = UIProgressView(progressViewStyle: .)
+		progressView = UIProgressView(progressViewStyle: .default)
 		progressView.sizeToFit()
 		let progressButton = UIBarButtonItem(customView: progressView)
 		
