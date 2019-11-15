978517d637bf54d5e2ae43e920c0898f685bf0fa
Noah Henry <noah.henry@allscripts.com>
2019-11-15 13:47:04 -0600

    project-04 challenge 2

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -25,16 +25,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		// Do any additional setup after loading the view.
 		
 		navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-		navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(goBack))
 		
 		let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
 		let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+		let goBack = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+		let goForward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
 		
 		progressView = UIProgressView(progressViewStyle: .default)
 		progressView.sizeToFit()
 		let progressButton = UIBarButtonItem(customView: progressView)
 		
-		toolbarItems = [progressButton, spacer, refresh]
+		toolbarItems = [progressButton, spacer, goBack, goForward, refresh]
 		navigationController?.isToolbarHidden = false
 		
 		webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
