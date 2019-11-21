dca8d2a5c6805ea3cdf3cf13ccaa630f1c987952
Noah Henry <noah.henry@allscripts.com>
2019-11-21 08:54:16 -0600

    project-05 pt. 2 - Checking for valid answers

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -79,14 +79,27 @@ class ViewController: UITableViewController {
 	}
 	
 	func isPossible(word: String) -> Bool {
+		guard var tempWord = title?.lowercased() else { return false }
+		
+		for letter in word {
+			if let position = tempWord.firstIndex(of: letter) {
+				tempWord.remove(at: position)
+			} else {
+				return false
+			}
+		}
+		
 		return true
 	}
 	
 	func isOriginal(word: String) -> Bool {
-		return true
+		return !usedWords.contains(word)
 	}
 	
 	func isReal(word: String) -> Bool {
-		return true
+		let checker = UITextChecker()
+		let range = NSRange(location: 0, length: word.utf16.count) // convert swfit string to a formatt acceptable by objective-c
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+		return misspelledRange.location == NSNotFound
 	}
 }
