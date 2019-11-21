9d8293d147b34ad9d8a4497030f54ef6899d80ec
Noah Henry <noah.henry@allscripts.com>
2019-11-21 13:24:45 -0600

    project-05 Challenge pt. 3

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UITableViewController {
 		// Do any additional setup after loading the view.
 		
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+		navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Restart", style: .plain, target: self, action: #selector(startGame))
 		
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
@@ -31,7 +32,7 @@ class ViewController: UITableViewController {
 		startGame()
 	}
 
-	func startGame() {
+	@objc func startGame() {
 		title = allWords.randomElement()
 		usedWords.removeAll(keepingCapacity: true)
 		tableView.reloadData()
