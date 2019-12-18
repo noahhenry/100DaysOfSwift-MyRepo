fb051ceac314e38ed1b6b859b01ead9d08d5ee4d
Noah Henry <noah.henry@allscripts.com>
2019-12-18 14:04:42 -0600

    project-07 Pt. 1 - Parsing JSON using the codable protocol

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Henry, Noah on 12/18/19.
+//  Copyright © 2019 hackingwithswift. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+	var results: [Petition]
+}
