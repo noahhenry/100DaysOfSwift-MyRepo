95edb8b9f41bdfea78d2d522d76116748fe843b3
Noah Henry <noah.henry@allscripts.com>
2019-12-19 13:24:45 -0600

    poject-07 Pt. 2 - Finishing touches: didFinishLaunchingWithOptions

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -16,15 +16,35 @@ class ViewController: UITableViewController {
 		// Do any additional setup after loading the view.
 		
 		// let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-		let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+		let urlString: String
+
+		if navigationController?.tabBarItem.tag == 0 {
+			// urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+			urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+		} else {
+			// urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+			urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+		}
 		
 		if let url = URL(string: urlString) {
 			if let data = try? Data(contentsOf: url) {
 				// we're OK to parse
 				parse(json: data)
+				
+				return
 			}
 		}
 		
+		showError()
+	}
+	
+	func showError() {
+		let ac = UIAlertController(title: "Loading Error",
+								   message: "There was a problem loading the feed; please check your connection and try again.",
+								   preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		
+		present(ac, animated: true)
 	}
 	
 	func parse(json: Data) {
