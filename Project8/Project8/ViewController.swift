776e4b7d1e7cceec19d5cb4caec88fdd6fb3f52d
Noah Henry <noah.henry@allscripts.com>
2020-01-07 09:34:39 -0600

    project-08 pt. 1 - initial setup

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Henry, Noah on 1/7/20.
+//  Copyright © 2020 hackingwithswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
